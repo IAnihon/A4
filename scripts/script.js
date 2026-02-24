7b070a3f07f43ae308771148294fdcf0f171acf2
Iftekhar Nihon <ifthakharnehon@gmail.com>
2026-02-24 23:43:18 +0600

    All done

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,49 @@ function getId(id){
 }
 
 
+// new code
+
+function view(){
+
+ if(currentStatus ==='all-btn'){
+  cardContainer.classList.remove('hidden');
+  noJobSection.classList.add('hidden');
+  filterSection.classList.add('hidden');
+ }
+ 
+ else if(currentStatus ==='reject-top-btn'){
+  
+  if(rejectedList.length == 0){
+    cardContainer.classList.add('hidden');
+    noJobSection.classList.remove('hidden');
+    filterSection.classList.add('hidden')
+  }
+  else{
+    cardContainer.classList.add('hidden');
+    filterSection.classList.remove('hidden');
+      noJobSection.classList.add('hidden');
+  }
+  renderReject()
+
+ }
+ else if(currentStatus ==='interview-top-btn'){
+  if(interviewList.length == 0){
+    cardContainer.classList.add('hidden');
+    noJobSection.classList.remove('hidden');
+    filterSection.classList.add('hidden');
+  }
+  else{
+    cardContainer.classList.add('hidden');
+    filterSection.classList.remove('hidden');
+     noJobSection.classList.add('hidden');
+  }
+  renderInterview()
+  
+
+ }
+
+}
+
 function toggle(id){
 allBtn.classList.remove('bg-blue-500', 'text-white');
 interviewBtn.classList.remove('bg-blue-500', 'text-white');
